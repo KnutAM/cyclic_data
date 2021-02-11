d42341d0f77ac57430943812809d44642570f7ac
Knut Andreas Meyer <knutam@gmail.com>
2021-02-11 18:47:48 +0100

    Removed data_path.py from tracking

--- cyclic_data/data_path.py
+++
@@ -1,2 +0,0 @@
-data_path = ("This string can contain jibberish because setup.py will overwrite this file that is not version controlled" + 
-             "However, this file and the definition of data_path is required for the Travis CI test runs where setup.py is not used")
