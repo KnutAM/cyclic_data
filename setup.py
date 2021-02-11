1e2cff46c4d191fa9c5f8dec2d21260c4e18ad9d
Knut Andreas Meyer <knutam@gmail.com>
2021-02-11 16:39:11 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='cyclic_data',
+      version='1.0',
+      description='Analysis and plotting of cyclic data',
+      author='Knut Andreas Meyer',
+      author_email='knutam@gmail.com',
+      url='https://cyclic-after-pdef-r260.readthedocs.io/en/latest/',
+      packages=['cyclic_data'],
+     )
