043c4da39a96dc5c79b455f5b5845f016d29a510
Knut Andreas Meyer <knutam@gmail.com>
2021-02-11 13:13:28 +0100

    Added loading of hdf5 data class

---
+++ cyclic_data/hdf5_data/load_hdf5_data.py
@@ -0,0 +1,67 @@
+import pathlib as pl
+import numpy as np
+import h5py
+
+default_data_file = pl.Path(__file__).parents[2].absolute() / 'data' / 'test_data.hdf5'
+
+
+class Hdf5Data:
+    def __init__(self, hdf5_data_file=default_data_file):
+        self.hdf = h5py.File(hdf5_data_file, 'r')
+
+    def get_data_by_attributes(self, attributes):
+        """ Get groups in the hdf5 data whose attributes match those provided to the function
+        :param attributes: Dictionary with attributes to search for
+        :type attributes: dict
+
+        :return: list of data and attributes for the found groups. The data format is given by get_data_by_group
+        :rtype: list[dict, dict]
+        """
+        data = []
+        attr = []
+        for grp_name in self.hdf:
+            group = self.hdf[grp_name]
+            attr_match = True
+            for key in attributes:
+                if group.attrs[key] != attributes[key]:
+                    attr_match = False
+
+            if attr_match:
+                tmp_data, tmp_attr = self.get_data_by_group(grp_name)
+                tmp_attr['group'] = grp_name
+                data.append(tmp_data)
+                attr.append(tmp_attr)
+
+        return data, attr
+
+    def get_data_by_group(self, grp_name):
+        """ Get the test data by group name
+        :param grp_name: The name of the group to get data for
+        :type grp_name: str
+
+        :return: A dictionary with data with the following keys:
+                 time, sig (normal stress), eps (normal strain),
+                 tau (shear stress), gam (shear strain),
+                 stp (step/cycle in the experiment)
+        :rtype: dict
+        """
+
+        if grp_name not in list(self.hdf.keys()):
+            print(grp_name + 'is not in hdf file')
+            return None, None
+
+        ri = self.hdf[grp_name].attrs['inner_diameter']/2.0
+        ro = self.hdf[grp_name].attrs['outer_diameter']/2.0
+        gl = self.hdf[grp_name].attrs['gauge_length']
+
+        area = np.pi*(ro**2 - ri**2)
+        rm = (ri + ro)/2.0
+
+        data = {'time': np.array(self.hdf[grp_name]['time']),
+                'sig': np.array(self.hdf[grp_name]['forc'])/area,
+                'eps': np.array(self.hdf[grp_name]['astr']),
+                'tau': np.array(self.hdf[grp_name]['torq'])/(area*rm),
+                'gam': np.array(self.hdf[grp_name]['tstr'])*rm/gl,
+                'stp': np.array(self.hdf[grp_name]['acnt']) + np.array(self.hdf[grp_name]['tcnt'])}
+
+        return data, dict(self.hdf[grp_name].attrs)
---
+++ test/test_hdf5/test_hdf5.py
@@ -0,0 +1,76 @@
+import sys
+import pathlib as pl
+import numpy as np
+
+# Add top level folder
+cyclic_data_dir = pl.Path(__file__).parents[2].absolute()
+sys.path.append(str(cyclic_data_dir))
+
+from cyclic_data.hdf5_data.load_hdf5_data import Hdf5Data
+
+hdf5_test_file = pl.Path(__file__).parent / 'test_file.hdf5'
+
+
+def test_test_file():
+    """ Verify that the Hdf5Data class can initialize correctly by opening the test file
+        This test isolates that the test file used to verify other functionalities is working
+    """
+    try:
+        hdf5 = Hdf5Data(hdf5_test_file)
+    except Exception as e:
+        assert False, "opening test file raised " + str(e)
+
+    assert isinstance(hdf5, Hdf5Data)
+
+
+def test_get_data_by_group():
+    existing_group = 'T01'
+    hdf5 = Hdf5Data(hdf5_test_file)
+    data, attr = hdf5.get_data_by_group(existing_group)
+    assert isinstance(data, dict)
+    assert 'time' in data
+    assert isinstance(data['time'], np.ndarray)
+    assert isinstance(attr, dict)
+    assert 'load_angle' in attr
+    assert 'group' not in attr, '"group" should not be in attributes when returned from get_by_group'
+
+    non_existing_group = 'non_existing'
+    data, attr = hdf5.get_data_by_group(non_existing_group)
+    assert data is None
+    assert attr is None
+
+
+def test_get_data_by_attributes():
+    hdf5 = Hdf5Data(hdf5_test_file)
+
+    # Test that function fails for an attribute not in the data
+    def verify_failure():
+        attr = {'load_angle': 0, 'non_existing_attr': ''}
+        try:
+            hdf5.get_data_by_attributes(attr)
+            print('Should exit with error if a non-existing attribute is requested')
+            return False
+        except KeyError:  # Should fail with KeyError if attribute not found in file
+            return True
+        except Exception as e:
+            print('Should give KeyError for non-existing attribute, but the following was thrown:')
+            print(e.__class__)
+            print(e)
+            return False
+
+    assert verify_failure()
+
+    # Test that a list is returned
+    # Test that the list has length 2 if 2 attributes match
+    attr = {'load_angle': 0, 'load_type': 'Proportional'}
+    data, attr = hdf5.get_data_by_attributes(attr)
+    assert isinstance(data, list)
+    assert isinstance(attr, list)
+    assert len(data) == len(attr)
+    assert len(data) == 2
+
+    # Test that the list is empty if no attributes match
+    attr = {'load_angle': 0, 'load_type': 'does_not_exist'}
+    data, attr = hdf5.get_data_by_attributes(attr)
+    assert len(data) == 0
+    assert len(attr) == 0
