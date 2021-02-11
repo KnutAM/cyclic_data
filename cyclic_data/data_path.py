df9e701c907f238520b54a468133f5e410c1dc52
Knut Andreas Meyer <knutam@gmail.com>
2021-02-11 18:40:45 +0100

    Create data_path.py

---
+++ cyclic_data/data_path.py
@@ -0,0 +1,2 @@
+data_path = ("This string can contain jibberish because setup.py will overwrite this file that is not version controlled" + 
+             "However, this file and the definition of data_path is required for the Travis CI test runs where setup.py is not used")
