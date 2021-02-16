1067a8cf59683c7e4d28364ab20959b7bf509cbd
Knut Andreas Meyer <knutam@gmail.com>
2021-02-16 21:07:53 +0100

    Added time-stamp to documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,7 +72,7 @@ pygments_style = None
 
 
 # -- Options for HTML output -------------------------------------------
-
+html_last_updated_fmt = "%b %d %y at %H:%M"
 # The theme to use for HTML and HTML Help pages.  
 # See the documentation for a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
