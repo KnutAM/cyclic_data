8a883a7e045c8363c498d4f323d4b1144d36191e
Knut Andreas Meyer <knutam@gmail.com>
2021-02-17 21:58:13 +0100

    Generalized mid-value function

--- cyclic_data/cycle.py
+++ cyclic_data/cycle.py
@@ -33,6 +33,42 @@ def get_pv_inds(test_data, num_per_cycle=2, num_ind_skip=0):
 def get_mid_values(test_data, qty, pv_inds):
     """ Get mid values of test_data[qt] for qt in qty. One value for each segment
     as descirbed by pv_inds
+    See :py:func:`get_segment_values` for a description of the input parameters
+
+    :returns: mid values for each quantity in qty as dictionary. The data are lists with
+              length len(pv_inds) containing numpy arrays
+    :rtype: dict
+    """
+
+    def average(peak, valley):
+        return (peak + valley)/2.0
+
+    return get_segment_values(test_data, qty, pv_inds, average)
+
+
+def get_diff_values(test_data, qty, pv_inds):
+    """ Get difference of test_data[qt] for qt in qty between adjacent
+    peaks and valleys. One value for each segment as descirbed by pv_inds.
+    See :py:func:`get_segment_values` for a description of the input parameters
+
+    :returns: difference values for each quantity in qty as dictionary. The data are lists with
+              length len(pv_inds) containing numpy arrays
+    :rtype: dict
+
+    """
+
+    def difference(peak, valley):
+        return peak - valley
+
+    return get_segment_values(test_data, qty, pv_inds, difference)
+
+
+def get_segment_values(test_data, qty, pv_inds, operation):
+    """ Get segment value of test_data[qt] for qt in qty between adjacent
+    peaks and valleys. The function ``operation`` is applied to the peak
+    and valley values of test_data[qt]. Note that it does not have to be peaks
+    or valleys as for uniaxial data, but characteristic points in the experiment
+    data (e.g. pv_inds can contain the valley followed by the yield point).
 
     :param test_data: test data as dictionary, must contain fields given by qty
     :type test_data: dict
@@ -44,6 +80,10 @@ def get_mid_values(test_data, qty, pv_inds):
                     Each outer list contain information about the same point type.
                     E.g. pv_inds[0] gives peak indices, pv_inds[1] gives valley indices.
     :type pv_inds: iterable[ iterable[ int ] ]
+
+    :param operation: function that performs operation on two numpy arrays, describing
+                      the value at two adjacent peak and valley
+    :type operation: function
 
     :returns: mid values for each quantity in qty as dictionary. The data are lists with
               length len(pv_inds) containing numpy arrays
@@ -59,8 +99,8 @@ def get_mid_values(test_data, qty, pv_inds):
     # has no end index, hence [:len(i2)]
     inds1 = [pv[:len(i2)] for pv, i2 in zip(pv_inds, inds2)]  # Start of segment
 
-    mid_data = {}
+    segment_data = {}
     for qt in qty:
-        mid_data[qt] = [(test_data[qt][i1] + test_data[qt][i2])/2.0 for i1, i2 in zip(inds1, inds2)]
+        segment_data[qt] = [operation(test_data[qt][i1], test_data[qt][i2]) for i1, i2 in zip(inds1, inds2)]
 
-    return mid_data
+    return segment_data
