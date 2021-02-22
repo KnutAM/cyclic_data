242e4497aeaf60f31e96d96f0e35c387ae370da0
Knut Andreas Meyer <knutam@gmail.com>
2021-02-22 12:28:56 +0100

    Fixed prediction function and modified interfaces

--- cyclic_data/filters/functions.py
+++ cyclic_data/filters/functions.py
@@ -17,56 +17,62 @@ def linear_segments(t, v, num_segments=20, t_pred=None):
     """ Smooth v-data by fitting num_segments equally spaced linear segments
     """
     assert len(t) > num_segments, 'number of segments cannot be more than number of datapoints'
-    return spline(t, v, patsy.bs, num_knots=num_segments, params={'deg': 1}, t_pred=t_pred)
+    return spline(t, v, patsy.bs, num_knots=num_segments, b_degree=1, t_pred=t_pred)
 
 
-def b_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
+def b_spline(t, v, knot_fraction=0.25, num_knots=None, t_pred=None):
     """ Smooth v-data using B-spline
     """
-    return spline(t, v, patsy.bs, knot_fraction, num_knots, params, t_pred=t_pred)
+    return spline(t, v, patsy.bs, knot_fraction, num_knots, t_pred=t_pred)
 
 
-def natural_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
+def natural_spline(t, v, knot_fraction=0.25, num_knots=None, t_pred=None):
     """ Smooth v-data using a natural cubic spline
     """
-    return spline(t, v, patsy.cr, knot_fraction, num_knots, params, t_pred=t_pred)
+    return spline(t, v, patsy.cr, knot_fraction, num_knots, t_pred=t_pred)
 
 
-def cubic_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
+def cubic_spline(t, v, knot_fraction=0.25, num_knots=None, t_pred=None):
     """ Smooth v-data using a cubic spline
     """
-    return spline(t, v, patsy.cc, knot_fraction, num_knots, params, t_pred=t_pred)
+    return spline(t, v, patsy.cc, knot_fraction, num_knots, t_pred=t_pred)
 
 
-def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
+def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, b_degree=None, t_pred=None):
     """ Smooth using a given spline basis with num_knots
-    If 'knots' in params, use those knot locations
-    If 'df" in params, set num_knots = params['df']
     If num_knots not given, set num_knots = floor(len(t)*knot_fraction)
     If t_pred given, return predicted values for different time coordinates
     """
-    params_ = {} if params is None else {key: params[key] for key in params if key != 'df'}
-    # Setup basis function, need to define knots explicitly for prediction to work correctly
+    # Set parameters if b_degree given (degree of b-spline)
+    params = {} if b_degree is None else {'degree': b_degree}
+
+    # Setup basis function, need to define knots and bounds explicitly for prediction to work correctly
     # (I.e. we need to use the same knots for prediction as for fitting!)
-    if 'knots' not in params:
-        if 'df' in params:
-            num_knots_ = params['df']
-        else:
-            num_knots_ = int(len(t) * knot_fraction) if num_knots is None else num_knots
-        knots = tuple(np.linspace(t[0], t[-1], num_knots_))
-        params_['knots'] = knots
+    # Get number of knots
+    num_knots_ = int(len(t) * knot_fraction) if num_knots is None else num_knots
+    # Distribute knots
+    knots = tuple(np.linspace(t[0], t[-1], num_knots_))
+    # Find bounds
+    t_min = np.min(t) if t_pred is None else min(np.min(t), np.min(t_pred))
+    t_max = np.max(t) if t_pred is None else max(np.max(t), np.max(t_pred))
+    # Expand bounds to avoid numerical issues (double precision)
+    t_min -= abs(t_min) * 1.e-1
+    t_max += abs(t_max) * 1.e-1
 
-    base_function = spline_basis(t, **params_)
+    # Construct base function for fitting
+    base_function = spline_basis(t, knots=knots, lower_bound=t_min, upper_bound=t_max, **params)
 
+    # Construct model for fitting
     model = patsy.dmatrix(base_function)
+
+    # Fit model
     fit = sm.GLM(v, model).fit()
 
-    if t_pred is None:
+    if t_pred is None:  # Return smoothened values by evaluating the model at the fitted data
         smoothened = fit.predict(model)
-    else:
-        pred_base = spline_basis(t_pred, **params_)
+    else:               # Use the fit to predict new time values
+        pred_base = spline_basis(t_pred, knots=knots, lower_bound=t_min, upper_bound=t_max, **params)
         pred_model = patsy.dmatrix(pred_base)
         smoothened = fit.predict(pred_model)
-        print('updated')
 
     return np.array(smoothened)
