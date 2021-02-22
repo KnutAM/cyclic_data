003bb0a937553f66fedbaa363b78c8ac450a99d5
Knut Andreas Meyer <knutam@gmail.com>
2021-02-22 09:24:51 +0100

    Added possibility of getting prediction output instead of fit for filters

--- cyclic_data/filters/functions.py
+++ cyclic_data/filters/functions.py
@@ -3,42 +3,46 @@ import patsy
 import statsmodels.api as sm
 
 
-def polynomial(t, v, deg=3):
+def polynomial(t, v, deg=3, t_pred=None):
     """ Smooth v-data using polynomial of given degree
     """
     c = np.polynomial.Polynomial.fit(t, v, deg=deg)
-    return np.polynomial.polynomial.polyval(c, t)
+    if t_pred is None:
+        return np.polynomial.polynomial.polyval(c, t)
+    else:
+        return np.polynomial.polynomial.polyval(c, t_pred)
 
 
-def linear_segments(t, v, num_segments=20):
+def linear_segments(t, v, num_segments=20, t_pred=None):
     """ Smooth v-data by fitting num_segments equally spaced linear segments
     """
     assert len(t) > num_segments, 'number of segments cannot be more than number of datapoints'
-    return spline(t, v, patsy.bs, num_knots=num_segments, params={'deg': 1})
+    return spline(t, v, patsy.bs, num_knots=num_segments, params={'deg': 1}, t_pred=t_pred)
 
 
-def b_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+def b_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
     """ Smooth v-data using B-spline
     """
-    return spline(t, v, patsy.bs, knot_fraction, num_knots, params)
+    return spline(t, v, patsy.bs, knot_fraction, num_knots, params, t_pred=t_pred)
 
 
-def natural_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+def natural_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
     """ Smooth v-data using a natural cubic spline
     """
-    return spline(t, v, patsy.cr, knot_fraction, num_knots, params)
+    return spline(t, v, patsy.cr, knot_fraction, num_knots, params, t_pred=t_pred)
 
 
-def cubic_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+def cubic_spline(t, v, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
     """ Smooth v-data using a cubic spline
     """
-    return spline(t, v, patsy.cc, knot_fraction, num_knots, params)
+    return spline(t, v, patsy.cc, knot_fraction, num_knots, params, t_pred=t_pred)
 
 
-def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None):
+def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None, t_pred=None):
     """ Smooth using a given spline basis with num_knots
     If 'df" in params, set num_knots = params['df']
     If num_knots not given, set num_knots = floor(len(t)*knot_fraction)
+    If t_pred given, return predicted values for different time coordinates
     """
     params = {} if params is None else params
     # Setup basis function
@@ -51,5 +55,10 @@ def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None):
     model = patsy.dmatrix(base_function, return_type='dataframe')
     fit = sm.GLM(v, model).fit()
 
-    return fit.predict(model)
+    if t_pred is None:
+        return fit.predict(model)
+    else:
+        pred_base = spline_basis(t_pred, **params) if 'df' in params else spline_basis(t_pred, df=df, **params)
+        pred_model = patsy.dmatrix(pred_base, return_type='dataframe')
+        return fit.predict(pred_model)
 
