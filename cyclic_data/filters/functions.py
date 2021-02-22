fea44e949d70b02ba91bd50b7d3a3f94e685ecd3
Knut Andreas Meyer <knutam@gmail.com>
2021-02-22 09:16:52 +0100

    Added polynomial and linear segment filtering functions

--- cyclic_data/filters/functions.py
+++ cyclic_data/filters/functions.py
@@ -1,20 +1,45 @@
+import numpy as np
 import patsy
 import statsmodels.api as sm
 
 
+def polynomial(t, v, deg=3):
+    """ Smooth v-data using polynomial of given degree
+    """
+    c = np.polynomial.Polynomial.fit(t, v, deg=deg)
+    return np.polynomial.polynomial.polyval(c, t)
+
+
+def linear_segments(t, v, num_segments=20):
+    """ Smooth v-data by fitting num_segments equally spaced linear segments
+    """
+    assert len(t) > num_segments, 'number of segments cannot be more than number of datapoints'
+    return spline(t, v, patsy.bs, num_knots=num_segments, params={'deg': 1})
+
+
 def b_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+    """ Smooth v-data using B-spline
+    """
     return spline(t, v, patsy.bs, knot_fraction, num_knots, params)
 
 
 def natural_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+    """ Smooth v-data using a natural cubic spline
+    """
     return spline(t, v, patsy.cr, knot_fraction, num_knots, params)
 
 
 def cubic_spline(t, v, knot_fraction=0.25, num_knots=None, params=None):
+    """ Smooth v-data using a cubic spline
+    """
     return spline(t, v, patsy.cc, knot_fraction, num_knots, params)
 
 
 def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None):
+    """ Smooth using a given spline basis with num_knots
+    If 'df" in params, set num_knots = params['df']
+    If num_knots not given, set num_knots = floor(len(t)*knot_fraction)
+    """
     params = {} if params is None else params
     # Setup basis function
     if 'df' in params:
@@ -27,3 +52,4 @@ def spline(t, v, spline_basis, knot_fraction=0.25, num_knots=None, params=None):
     fit = sm.GLM(v, model).fit()
 
     return fit.predict(model)
+
